6b2fe1be2172942ebb6f7e7d013c5b779656b61b
saurabh singh <ss1380820@gmail.com>
2026-07-17 20:05:41 +0530

    microservices with nginx and horizontal scaling

---
+++ level4/phase2/backend/gateway/index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import dotenv from "dotenv";
+import proxy from "express-http-proxy";
+
+dotenv.config();
+
+const port=process.env.PORT || 3000;
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: `hello from ${process.env.SERVER_NAME}`
+    })
+})
+
+app.use('/auth',proxy('http://auth:3000'))
+app.use('/order',proxy('http://order:3001'))
+app.use('/product',proxy('http://product:3002'))
+
+
+app.listen(port,()=>{
+    console.log(`server is running on port ${port}`)
+})
+
+
+
---
+++ level4/phase2/backend/services/order/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+const port=process.env.PORT || 3000;
+
+const app=express();
+
+app.get('/',(req,res)=>{
+    res.status(200).json({
+               message:`hello from {process.env.SERVER_NAME} microservice`
+
+    })
+})
+
+app.listen(port,()=>{
+    console.log(`server is running on port ${port}`)
+})
